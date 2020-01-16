618fe6ba5ef680c253e5ae1e4e8cc5084f3d7fff
黄锐 <hr209059@alibaba-inc.com>
2020-01-16 15:52:09 +0800

    feat: ora 转圈圈

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk'); // https://www.npmjs.com/package/chalk
 const inquirer = require('inquirer'); // https://www.npmjs.com/package/inquirer
 const download = require('download-git-repo');// https://www.npmjs.com/package/download-git-repo
+const ora = require('ora');// https://www.npmjs.com/package/ora
 
 inquirer.prompt(
   [
@@ -15,9 +16,13 @@ inquirer.prompt(
   .then(
     answers => {
       if (answers.OK === true) {
+        const spinner = ora('正在下载').start();
         download(
           'flippidippi/download-git-repo-fixture', 'test/tmp', function (err) {
-            console.log(chalk.blue(err ? 'Error' : 'Success'))
+              err ?
+                spinner.succeed(chalk.green('下载成功'))
+              :
+                spinner.fail(chalk.red('下载失败'))
           }
         )
       }
